b8750953d03ef1cbdb7815f258cd17bfc37592ce
Flor Pocetti <pochi@Flors-MacBook-Pro.local>
2024-01-15 23:08:13 -0300

    adding pagination and improving styles

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
 /* 
-1. Indicar en qué HTML se cargan los elementos de la URL
-2. Indicar la interacción con el Load more button
-3. Paginado (número de página e items por página)
+- Indicar en qué HTML se cargan los elementos de la URL
+- Cargar los primeros 6 elementos
+- Indicar la interacción con el Load more button
+- Paginado (número de página e items por página)
 
  */
 
 import axios from "https://cdn.skypack.dev/axios";
 
 let productsArray;
-let currentPage = 1;
-let itemsPerPage = 9;
+let currentPage = 0;
+let itemsPerPage = 6;
+let productsURL = "https://dummyjson.com/products";
 
 //indico el container de los productos
 const productCardContainer = document.getElementById("products-container");
 
-//me traigo los productos de la url
-const getProducts = async () => {
+//guardo los productos en una única axios call
+const getAllProducts = async () => {
   try {
-    const response = await axios.get("https://dummyjson.com/products");
-    let productsArray = response.data.products;
-    console.log("resultado del axios ", productsArray);
-
-    //inserto una card por cada producto que me traigo de la url
-    productsArray.forEach((product) => {
-      const productCard = createProductCard(product);
-      productCardContainer.appendChild(productCard);
-    });
-  } catch (error) {
+    const response = await axios.get(productsURL);
+    productsArray = response.data.products;
+  } catch {
     console.error(error);
   }
+
+  getInitialProducts(productsArray);
+  return productsArray;
+};
+
+const getInitialProducts = (productsArray) => {
+  console.log(productsArray);
+
+  for (let i = 0; i < itemsPerPage; i++) {
+    const productCard = createProductCard(productsArray[i]);
+    productCardContainer.appendChild(productCard);
+    // console.log("check ", i);
+  }
+  return ++currentPage;
+};
+
+//me traigo más productos de la url
+const getProducts = (productsArray) => {
+  console.log("This is the current page ", currentPage);
+  let initialProduct = currentPage * itemsPerPage;
+  console.log(initialProduct);
+  let finalProduct = itemsPerPage * (currentPage + 1);
+  console.log(finalProduct);
+
+  //inserto un nuevo set de cards por cada click al boton
+
+  for (let i = initialProduct; i < finalProduct; i++) {
+    const productCard = createProductCard(productsArray[i]);
+    productCardContainer.appendChild(productCard);
+  }
+  return ++currentPage;
 };
 
 //Recreo la card con cada producto que me traigo de la url
 const createProductCard = (product) => {
   const productCard = document.createElement("div");
   productCard.classList.add("product");
-  productCard.innerHTML = `<img src="${product.images[0]}" alt="${product.name}" />
-    <h3>"${product.title}"</h3>
-    <p>"${product.description}"</p>
+  productCard.innerHTML = `
+  <div class='productHead'>
+    <div class="productImage">
+    <img src=${product.images[0]} alt=${product.name} />
+    </div>
+    <h3>${product.title}</h3>
+    <p>${product.description}</p>
+  </div>
     <div class="price">
       <p>
         € <a>${product.price}</a>
@@ -54,7 +85,6 @@ const createProductCard = (product) => {
 };
 
 const loadButton = document.getElementById("load-btn");
+loadButton.addEventListener("click", () => getProducts(productsArray));
 
-loadButton.addEventListener("click", () => {
-  getProducts();
-});
+document.addEventListener("DOMContentLoaded", getAllProducts());
